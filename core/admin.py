ebe319a2374378a4bb499c93ac37d93df349e834
Michał Brejnak <michas.brejnak22@gmail.com>
2023-11-09 23:12:24 +0100

    django custom user

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,8 @@
+import django.contrib.auth.admin
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(django.contrib.auth.admin.UserAdmin):
+    pass
