c50120d975f0d8c9b2c0d716f8c75d89904111ba
Michał Brejnak <michas.brejnak22@gmail.com>
2023-11-10 04:03:20 +0100

    change User to Account, initial core migration changed

--- core/admin.py
+++ core/admin.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 from .models import *
 
 
-@admin.register(User)
+@admin.register(Account)
 class UserAdmin(django.contrib.auth.admin.UserAdmin):
     pass
--- core/models/auth.py
+++ core/models/auth.py
@@ -2,5 +2,5 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
-class User(AbstractUser):
+class Account(AbstractUser):
     pass
