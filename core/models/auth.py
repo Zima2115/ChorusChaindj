3a0f37e39f2f94169928df1a793c92aa17d9498c
Michał Brejnak <michas.brejnak22@gmail.com>
2023-11-10 04:31:30 +0100

    change Account username field to email

--- core/models/auth.py
+++ core/models/auth.py
@@ -1,6 +1,24 @@
+from django.contrib.auth.validators import UnicodeUsernameValidator
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
 class Account(AbstractUser):
-    pass
+    USERNAME_FIELD = 'email'
+
+    email = models.EmailField("email address", unique=True)
+    username_validator = UnicodeUsernameValidator()
+
+    username = models.CharField(
+        "username",
+        max_length=150,
+        unique=True,
+        validators=[username_validator],
+        error_messages={
+            "unique": "This username is taken.",
+        },
+        null=True,
+        blank=True
+    )
+
+    REQUIRED_FIELDS = []
